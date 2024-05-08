02f9d71d64fe702edf40537202985752fb31a441
Alejandro Montenegro Taborda <amontenegrot@unal.edu.co>
2024-05-08 05:25:54 -0500

    Refactoring: cibersecurity package

--- codigo/utilities/cibersecurity.py
+++ codigo/utilities/cibersecurity.py
@@ -1,8 +1,16 @@
 import uuid
 
 
-# Generate a UUID random or based at id
-def uuid_generator(id):
+def uuid_generator(id: str) -> uuid.UUID:
+    """
+    Generate a UUID based on the given ID using UUID version 5.
+
+    Args:
+        id (str): The input ID to generate the UUID.
+
+    Returns:
+        UUID: The UUID generated based on the input ID.
+    """
     namespace = uuid.NAMESPACE_URL
     id = str(id)
     uuid_from_name = uuid.uuid5(namespace, id)
